b9e868c83f6bffb69019163cd9a549a2c17696f7
Danduriel <meckzankzwerg@yahoo.de>
2018-02-23 16:17:14 +0100

    init

---
+++ android/src/app/flunky/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package app.flunky;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import app.flunky.Orchestrator;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Orchestrator(), config);
+	}
+}
